c028f256e2a4e87e2b360039968107ba7d0de277
chintanparikh <chintanparikh@gatech.edu>
2013-06-06 16:22:10 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThunderMountain::Application.config.secret_token = '5fe57cbb79e99c229ac3a4391db83562bffbb6b599f0ece073291d6f5fb28cc7e44b889934e602b5fcb69d68c6ce76c880cef28455388860fe202e8d225a1e8c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ThunderMountain::Application.config.session_store :cookie_store, key: '_ThunderMountain_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ThunderMountain::Application.config.session_store :active_record_store
