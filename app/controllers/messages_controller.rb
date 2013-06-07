63f5eef040c51650662d2287924205ddb775e7e4
chintanparikh <chintanparikh@gatech.edu>
2013-06-07 15:35:48 -0400

    move from erb to haml

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,7 @@
 class MessagesController < ApplicationController
   def index
+    #should probably move the new and index into different actions, but it's such a small app it's more convenient like this
+    @message = Message.new
     @messages = Message.all
   end
 
